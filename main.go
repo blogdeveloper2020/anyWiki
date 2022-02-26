15127b2f8c4a26e44db43cd53e819ae7866682c5
bookstoreTest <golangblog@protonmail.com>
2022-02-26 21:53:50 +0800

    html response

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type IndexData struct {
@@ -20,11 +22,25 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonStr)
 }
 
+func indexHtml(w http.ResponseWriter, r *http.Request) {
+	var indexData IndexData
+	indexData.Title = "anyWiki"
+	indexData.Desc = "你好,欢迎来到anyWiki"
+	t := template.New("index.html")
+	//1.get current path
+	path, _ := os.Getwd()
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	//2.output the indexData to html file
+	t.Execute(w, indexData)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8800",
 	}
 	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", indexHtml)
+
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
