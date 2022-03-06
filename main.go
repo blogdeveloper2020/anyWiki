46cdf38bffb3f18d103824d987c900bb73b2773c
bookstoreTest <golangblog@protonmail.com>
2022-03-06 20:02:50 +0800

    首页假数据填充

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type IndexData struct {
@@ -13,6 +14,16 @@ type IndexData struct {
 	Desc  string `json:"desc"`
 }
 
+func IsODD(num int) bool {
+	return num%2 == 0
+}
+func GetNextName(strs []string, index int) string {
+	return strs[index+1]
+}
+
+func Date(layout string) string {
+	return time.Now().Format(layout)
+}
 func index(w http.ResponseWriter, r *http.Request) {
 	var indexData IndexData
 	indexData.Title = "anyWiki"
@@ -25,13 +36,43 @@ func index(w http.ResponseWriter, r *http.Request) {
 	footer := path + "/template/layout/footer.html"
 	personal := path + "/template/layout/personal.html"
 	pagination := path + "/template/layout/pagination.html"
-	poster := path + "/template/layout/poster.html"
-
-	t, _ = t.ParseFiles(path+"/template/index.html",
-		home, header, footer, personal, pagination, poster)
+	postList := path + "/template/layout/post-list.html"
+	t.Funcs(template.FuncMap{"isODD": IsODD, "getNextName": GetNextName, "date": Date})
+	t, err := t.ParseFiles(path+"/template/index.html",
+		home, header, footer, personal, pagination, postList)
+	if err != nil {
+		log.Println(err)
+	}
 	//2.output the indexData to html file
-	viewer := config.Viewer{}
-	var hr = &models.HomeResponse{}
+	var categories = []models.Category{
+		{
+			Cid:  1,
+			Name: "go",
+		},
+	}
+
+	var posts = []models.PostMore{
+		{
+			Pid:          1,
+			Title:        "AnyWiki",
+			Content:      "内容",
+			UserName:     "tom",
+			ViewCount:    123,
+			CreateAt:     "2022-02-20",
+			CategoryId:   1,
+			CategoryName: "go",
+			Type:         0,
+		},
+	}
+	var hr = &models.HomeResponse{
+		config.Cfg.Viewer,
+		categories,
+		posts,
+		1,
+		1,
+		[]int{1},
+		true,
+	}
 	t.Execute(w, hr)
 }
 
